d7b46a300c648177da6d6bd69715bcdef7dafd87
tiboedturtels <u0927597@utah.edu>
2017-09-19 10:35:35 -0600

    Added backward shoot and minor tweaks

--- KickshotProject/Assets/Scripts/PlayerController.cs
+++ KickshotProject/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float KickSpeed;
-    public float StartGravity;
-    public float ChargeSpeed;
+    public float KickSpeed = 50;
+    public float FireDelay = 0.5f;
+    public float StartGravity = 50;
+    public float ChargeSpeed = 10;
 
     Transform _view_camera;
     Rigidbody _rigid_body;
 
     float m_charge;
+    bool _can_fire = true;
 
 	// Use this for initialization
 	void Start () {
@@ -27,18 +29,21 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(_can_fire && Input.GetButtonDown("Fire1"))
         {
-            FireProjectile();
-            ApplyKick();
+            FireProjectile(true);
+            ApplyKick(false);
+            StartCoroutine("ShootCooldown");
         }
-        if (Input.GetButton("Fire2"))
+        if (_can_fire && Input.GetButtonDown("Fire2"))
         {
-            ChargeWeapon();
+            FireProjectile(false);
+            ApplyKick(true);
+            StartCoroutine("ShootCooldown");
         }
         else
         {
-            ApplyKick(m_charge);
+            //ApplyKick(m_charge);
             m_charge = 0;
         }
         if (Input.GetButtonUp("Jump"))
@@ -60,28 +65,38 @@ public class PlayerController : MonoBehaviour {
     }
 
     // Launches the projectile
-    void FireProjectile()
+    void FireProjectile(bool forward)
     {
+        int dirFactor = forward ? 1 : -1;
+
         RaycastHit bulletHit = new RaycastHit();
-        if (Physics.Raycast(_view_camera.position, _view_camera.forward, out bulletHit)) ;
+        if (Physics.Raycast(_view_camera.position, dirFactor * _view_camera.forward, out bulletHit)) ;
             // Debug.Log("Bullet Hit at:" + bulletHit.point);
     }
 
     // Applies the recoil to this GameObject's Rigidbody
-    void ApplyKick()
+    void ApplyKick(bool forward)
     {
-        ApplyKick(KickSpeed);
+        ApplyKick(KickSpeed, forward);
     }
 
     // Overloads ApplyKick with a custom 'Speed'
-    void ApplyKick(float Speed)
+    void ApplyKick(float Speed, bool forward)
     {
-        Vector3 kickVel = -1 * _view_camera.forward * Speed;
+        int dirFactor = forward ? 1 : -1;
+        Vector3 kickVel = dirFactor * _view_camera.forward * Speed;
         Vector3 currentVel = GetComponent<Rigidbody>().velocity;
 
         // if (Vector3.Dot(kickVel, currentVel) < 0)
         //    currentVel = Vector3.zero;
         
         GetComponent<Rigidbody>().velocity = currentVel + kickVel;
+    }
+
+    IEnumerator ShootCooldown()
+    {
+        _can_fire = false;
+        yield return new WaitForSeconds(FireDelay);
+        _can_fire = true;
     }
 }
