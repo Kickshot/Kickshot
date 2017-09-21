9f551f5d8498c6d4cba3535e152e713ebc09065e
tiboedturtels <u0927597@utah.edu>
2017-09-21 15:34:41 -0600

    Testing scene is stable again.

---
+++ KickshotProject/Assets/Scripts/NoRigidBodyPC.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRigidBodyPC : MonoBehaviour {
+
+    public GameObject _camera;
+    
+    void Update () {
+        GetComponent<CharacterController>().SimpleMove(_camera.transform.forward * 10);
+	}
+}
