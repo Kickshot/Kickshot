57cd3762693e7ce2819b59c11c43070ad89d5718
Dalton Nell <naelstrof@gmail.com>
2017-10-15 00:29:43 -0600

    Added a surf_kitsune port (wip)

---
+++ KickshotProject/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+	public Transform teleportExit;
+	void OnTriggerEnter( Collider other ) {
+		SourcePlayer p = other.gameObject.GetComponent<SourcePlayer> ();
+		if (p != null) {
+			other.gameObject.transform.position = teleportExit.position;
+			other.gameObject.transform.rotation = teleportExit.rotation;
+		}
+	}
+}
