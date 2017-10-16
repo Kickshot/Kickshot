7777b3ae1329cd7bb2c8978e1f1629b6c2f0c999
Dalton Nell <naelstrof@gmail.com>
2017-10-16 02:33:19 -0600

    Accumulate camera impacts

--- KickshotProject/Assets/Scripts/SourcePlayer/MouseLook.cs
+++ KickshotProject/Assets/Scripts/SourcePlayer/MouseLook.cs
@@ -16,7 +16,7 @@ public class MouseLook : MonoBehaviour {
 	private Vector3 shakeIntensity;
 
 	public void ShakeImpact (Vector3 intensity) {
-		shakeIntensity = intensity/3f;
+		shakeIntensity += intensity/3f;
 		shakeTimer = intensity.magnitude/3f;
 		shakeTimerMax = intensity.magnitude/3f;
 	}
