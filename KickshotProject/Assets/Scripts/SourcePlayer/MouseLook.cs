224b5e4f27ca547dee539182e1f612296258b0bf
Dalton Nell <naelstrof@gmail.com>
2017-10-22 14:43:11 -0600

    MouseLook is no longer fps dependent

--- KickshotProject/Assets/Scripts/SourcePlayer/MouseLook.cs
+++ KickshotProject/Assets/Scripts/SourcePlayer/MouseLook.cs
@@ -6,8 +6,8 @@ public class MouseLook : MonoBehaviour {
     public Transform  view;
     private Vector3 viewOffset;
     private Quaternion offset;
-    public float xMouseSensitivity = 30.0f;
-    public float yMouseSensitivity = 30.0f;
+    public float xMouseSensitivity = 35.0f;
+    public float yMouseSensitivity = 35.0f;
     public float rotX;
     public float rotY;
     private Vector3 shakePos;
@@ -56,8 +56,8 @@ public class MouseLook : MonoBehaviour {
         }
 
         view.localPosition = viewOffset+shakePos;
-        rotX -= Input.GetAxis("Mouse Y") * xMouseSensitivity * 0.02f;
-        rotY += Input.GetAxis("Mouse X") * yMouseSensitivity * 0.02f;
+        rotX -= Input.GetAxis("Mouse Y") * xMouseSensitivity * Time.deltaTime;
+        rotY += Input.GetAxis("Mouse X") * yMouseSensitivity * Time.deltaTime;
         if (rotX < -90f) {
             rotX = -90f;
         } else if (rotX > 90f) {
