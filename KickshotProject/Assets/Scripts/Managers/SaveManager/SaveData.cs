a05c389f29c7d53bad1435fda5eedf05471d6a95
momo1478 <momo1478>
2017-10-23 02:23:35 -0600

    Save Manager can Write SaveData to a file!
    Use SaveManager.WriteState();

--- KickshotProject/Assets/Scripts/Managers/SaveManager/SaveData.cs
+++ KickshotProject/Assets/Scripts/Managers/SaveManager/SaveData.cs
@@ -10,4 +10,12 @@ public class SaveData {
     public virtual GameObject Load () {
         return null;
     }
+    // Saved Data must be writable to a json file.
+    public virtual string Serialize(){
+        return JsonUtility.ToJson(this);
+    }
+    // Save Data must also be readable from a file. MUST BE FILLED OUT BY CHILD CLASSES *ABSTRACT*
+    public virtual object Deserialize(string json){
+        return null;
+    }
 }
