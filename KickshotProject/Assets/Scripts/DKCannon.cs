b95c31e5c5e5be170dda4d36cf6bc35392c9ba41
Dalton Nell <naelstrof@gmail.com>
2017-10-24 12:56:07 -0600

    DKMountain!

---
+++ KickshotProject/Assets/Scripts/DKCannon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DKCannon : MonoBehaviour {
+    public float strength = 200f;
+    void OnTriggerEnter( Collider other ) {
+        if (other.tag == "Player") {
+            other.gameObject.GetComponent<SourcePlayer> ().velocity = transform.forward * strength;
+        }
+    }
+}
