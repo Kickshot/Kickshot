4cc446f3e282d941d40af6265368deb32b1f41db
John Radford <johnrad2825@gmail.com>
2017-11-03 22:25:51 -0600

    Added Minigun

---
+++ KickshotProject/Assets/Scripts/Guns/MiniGun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGun : GunBase {
+	private AudioSource blam;
+	public float strength;
+	void Start() {
+		blam = GetComponent<AudioSource> ();
+	}
+	override public void Update() {
+		base.Update ();
+		if (!equipped) {
+			return;
+		}
+		transform.rotation = view.rotation;
+	}
+	public override void OnPrimaryFire() {
+		blam.Play ();
+
+
+		player.velocity -= view.forward * strength;
+	}
+}
