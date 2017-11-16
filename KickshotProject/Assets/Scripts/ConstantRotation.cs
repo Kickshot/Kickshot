1240fa11b159143a762a1d6edce6bd8ba1c5d6c3
tiboedturtels <u0927597@utah.edu>
2017-11-15 23:00:47 -0700

    Added an easy script for constant rotation with gizmos.

---
+++ KickshotProject/Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ConstantRotation : MonoBehaviour {
+    public float CycleTime = 3f;
+    public bool Reverse = false;
+    public Transform AxisTarget;
+
+    Rigidbody body;
+    Vector3 rotAxis;
+
+    void Start()
+    {
+        rotAxis = (AxisTarget.transform.localPosition).normalized;
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        float degPerSecond = 360 / CycleTime;
+        degPerSecond *= Reverse ? -1 : 1; 
+        Quaternion delta = Quaternion.AngleAxis(degPerSecond * Time.deltaTime, rotAxis);
+        body.MoveRotation(transform.rotation * delta);
+	}
+
+    void Reset()
+    {
+        body = GetComponent<Rigidbody>();
+        body.useGravity = false;
+        body.isKinematic = true;
+        body.constraints = RigidbodyConstraints.FreezePosition;
+        body.interpolation = RigidbodyInterpolation.Interpolate;
+
+        AxisTarget = transform.Find("RotationAxis");
+        if (AxisTarget == null)
+        {
+            Object t = Resources.Load("EditorGizmos/RotationAxis");
+            AxisTarget = (Instantiate(t, transform.position + 3 * Vector3.up, Quaternion.identity, transform) as GameObject).transform;
+            AxisTarget.gameObject.name = "RotationAxis";
+        }
+    }
+}
---
+++ KickshotProject/Assets/Scripts/Gizmos/RotationAxisGizmo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAxisGizmo : MonoBehaviour {
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.parent.position);
+    }
+}
