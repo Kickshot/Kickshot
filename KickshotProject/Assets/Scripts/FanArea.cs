76096bc4ad7fe837e22c7c26998eaa23129623c8
tiboedturtels <u0927597@utah.edu>
2017-11-16 00:17:09 -0700

    Added fan prefab

---
+++ KickshotProject/Assets/Scripts/FanArea.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanArea : MonoBehaviour {
+    public float FanStrength = 50;
+
+    void OnTriggerStay(Collider other)
+    {
+        SourcePlayer sp = other.GetComponent<SourcePlayer>();
+        if (sp != null)
+        {
+            sp.velocity += (FanStrength * Time.deltaTime) * transform.up;
+        }
+    }
+}
