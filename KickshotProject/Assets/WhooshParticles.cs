d0d68c88565f9fd5af435165e44ec6d3c78df627
tiboedturtels <u0927597@utah.edu>
2017-12-08 02:49:48 -0700

    Added anime speed lines to the player

---
+++ KickshotProject/Assets/WhooshParticles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class WhooshParticles : MonoBehaviour {
+    public SourcePlayer _player;
+    public float _startSpeedThreshold = 10;
+    public float _maxSpeed = 40f;
+
+    ParticleSystem _particles;
+
+    private void Start()
+    {
+        _particles = GetComponent<ParticleSystem>();
+    }
+
+
+	void Update () {
+        float speed = _player.velocity.magnitude;
+        float whooshScale = Mathf.Clamp01((speed - _startSpeedThreshold) / (_maxSpeed - _startSpeedThreshold));
+
+        // Played around with rotating emitter with velocity, but doesn't feel right
+        //transform.rotation = Quaternion.FromToRotation(Vector3.forward, _player.velocity.normalized);
+
+        Color c = _particles.main.startColor.color;
+        var m = _particles.main;
+        m.startColor = new Color(c.r, c.g, c.b, whooshScale * whooshScale);
+	}
+}
