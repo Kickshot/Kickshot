97b6449aa083445a44bb00e5685844b37bfd6e7c
Connor Douglas <cjdouglas96@gmail.com>
2018-01-16 18:42:01 -0700

    Game Loop

---
+++ KickshotProject/Assets/GameLoop/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public string levelsScene;
+
+    public void Play() {
+        SceneManager.LoadScene(levelsScene);
+    }
+
+    public void Options() {
+        Debug.Log("Options Trigger");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
