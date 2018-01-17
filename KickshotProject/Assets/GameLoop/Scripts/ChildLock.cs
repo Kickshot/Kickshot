2b8d3d102c76f0f06c60e43d1da3b92aa909fe4c
Connor Douglas <cjdouglas96@gmail.com>
2018-01-16 21:00:34 -0700

    Add fancy ass UI

---
+++ KickshotProject/Assets/GameLoop/Scripts/ChildLock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ChildLock : MonoBehaviour {
+
+    private Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    private void Awake()
+    {
+        pos = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = pos;
+    }
+}
