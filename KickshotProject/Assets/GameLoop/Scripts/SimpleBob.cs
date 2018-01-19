bd1a0ae8d8c8ad7e1f4c678a70e510538a950032
Connor Douglas <cjdouglas96@gmail.com>
2018-01-19 00:32:48 -0700

    Floating Islands Level1 Update

---
+++ KickshotProject/Assets/GameLoop/Scripts/SimpleBob.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBob : MonoBehaviour {
+
+    [Range (0f, 2f)]
+    public float bobDistance;
+    [Range (0f, 2f)]
+    public float cycleSpeed;
+
+    private float offset;
+
+    private void Start()
+    {
+        offset = Random.Range(0f, 5f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.down * bobDistance * Time.deltaTime * Mathf.Sin((Time.time + offset) / cycleSpeed));
+    }
+}
