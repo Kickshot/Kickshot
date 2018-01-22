4b556461b024a4138cf0e9183ccfc5fd844aedbd
domaspiragas <domas.piragas@gmail.com>
2018-01-22 14:40:59 -0700

    ui-development: Start splash screen, new button.

---
+++ KickshotProject/Assets/Misc/Placeholder_UI/Scripts/PlaySplash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class PlaySplash : MonoBehaviour {
+
+    public VideoPlayer vp;
+
+	// Use this for initialization
+	void Awake () {
+        vp.Play();
+        vp.loopPointReached += LoadMainMenu;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void LoadMainMenu( VideoPlayer vp)
+    {
+        //TODO: Load in Main Menu here.
+        //SceneManager.LoadScene(1);
+    }
+}
