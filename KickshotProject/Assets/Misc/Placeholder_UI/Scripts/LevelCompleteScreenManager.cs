49dd45de2b2a632057f12a33a50e371da5a4bda7
domaspiragas <domas.piragas@gmail.com>
2018-01-24 17:00:17 -0700

    New Pause Menu. Level Complete Screen.

--- KickshotProject/Assets/Misc/Placeholder_UI/Scripts/LevelCompleteScreenManager.cs
+++ KickshotProject/Assets/Misc/Placeholder_UI/Scripts/LevelCompleteScreenManager.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelCompleteScreenManager : MonoBehaviour {
 
     // used to disable camera/movement
     public GameObject player, levelCompleteScreen, gun;
+    public Text HUDTime, completeTime;
+
+    void Start()
+    {
+        //player = GameObject.Find("SourcePlayer");
+       // DisplayLevelCompleteMenu();
+    }
 
     void Update()
     {
@@ -17,21 +25,26 @@ public class LevelCompleteScreenManager : MonoBehaviour {
     }
     public void DisplayLevelCompleteMenu()
     {
+        Time.timeScale = 0;
         player.GetComponent<MouseLook>().enabled = false;
         player.GetComponent<CharacterController>().enabled = false;
         player.GetComponent<SourcePlayer>().enabled = false;
         gun.SetActive(false);
         levelCompleteScreen.SetActive(true);
-        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        completeTime.text = HUDTime.text;
+        HUDTime.enabled = false;
     }
 
     public void ClickMainMenu()
     {
-        SceneManager.LoadScene("MainMenuIslands");
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void ClickNextLevel()
     {
+        Time.timeScale = 1;
         Debug.Log("Next Level To Be Implemented");
     }
 }
