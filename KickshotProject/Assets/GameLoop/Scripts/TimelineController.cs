b40ea4b785b142fa75093870ef02508af12002b7
Connor Douglas <cjdouglas96@gmail.com>
2018-01-25 17:52:38 -0700

    Intro scene

---
+++ KickshotProject/Assets/GameLoop/Scripts/TimelineController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineController : MonoBehaviour {
+
+    public List<GameObject> disableds;
+    public List<GameObject> enableds;
+
+    public PlayableDirector dir;
+
+    private void Start()
+    {
+
+        foreach (GameObject o in enableds)
+        {
+            o.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (dir.state != PlayState.Playing) {
+            StartGame();
+        }
+    }
+
+    private void StartGame() {
+        Debug.Log("Timeline over");
+        foreach (GameObject o in disableds) {
+            o.SetActive(false);
+        }
+        foreach (GameObject o in enableds) {
+            o.SetActive(true);
+        }
+    }
+}
