a6d55793afb85d1b96143ba14ad34fced1c8d665
Connor Douglas <cjdouglas96@gmail.com>
2018-01-26 11:49:50 -0700

    Add new Rocket Launcher model

---
+++ KickshotProject/Assets/FallAway.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallAway : MonoBehaviour {
+
+    public Rigidbody fallRigid;
+
+    private void Start()
+    {
+        fallRigid.isKinematic = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        fallRigid.isKinematic = false;
+    }
+}
