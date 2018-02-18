54284e4b45c70e8edc1efc558fd183954becede5
GregDaniels813 <drichman813@gmail.com>
2018-02-18 12:54:09 -0700

    Updated acorn particle effect

---
+++ KickshotProject/Assets/FloatingAcorn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingAcorn : MonoBehaviour {
+
+    public float degreesPerSecond = 60.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1.0f;
+
+    Vector3 positionOffset = new Vector3();
+    Vector3 temporaryPosition = new Vector3();
+
+	// Use this for initialization
+	void Start () {
+        positionOffset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+
+        temporaryPosition = positionOffset;
+        temporaryPosition.y += Mathf.Sin((Time.fixedTime)/2 * Mathf.PI * frequency) * amplitude;
+
+        transform.position = temporaryPosition;
+	}
+}
---
+++ KickshotProject/Assets/FloatingSparkles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingSparkles : MonoBehaviour {
+
+    public float amplitude = 0.5f;
+    public float frequency = 1.0f;
+
+    Vector3 positionOffset = new Vector3();
+    Vector3 temporaryPosition = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+        positionOffset = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        temporaryPosition = positionOffset;
+        temporaryPosition.y += Mathf.Sin((Time.fixedTime) / 2 * Mathf.PI * frequency) * amplitude;
+
+        transform.position = temporaryPosition;
+    }
+}
