95d27afe9dd4e02aca919cb83a91c3306d3b98e5
Monish Gupta <monish1478@gmail.com>
2018-02-22 15:39:22 -0700

    option menu just pops up.

--- KickshotProject/Assets/Scripts/UI/CanvasFade.cs
+++ KickshotProject/Assets/Scripts/UI/CanvasFade.cs
@@ -9,36 +9,45 @@ public class CanvasFade : MonoBehaviour {
 
     private CanvasGroup cGroup;
     private float curAlpha;
-    private bool faded = false;
+    private bool faded = true;
     private float targetAlpha;
 
     private float timer = 0f;
 
     private void Start()
     {
+        gameObject.SetActive(false);
         cGroup = GetComponent<CanvasGroup>();
         if (Mathf.Approximately(cGroup.alpha, 0f))
             faded = true;
     }
 
-    public void ToggleFade() {
-        if (faded) {
+    public void ToggleFade()
+    {
+        if (faded)
+        {
             faded = false;
             targetAlpha = 1f;
-        } else {
+            gameObject.SetActive(true);
+        }
+        else
+        {
             faded = true;
             targetAlpha = 0f;
         }
+        
         StartCoroutine(Fade());
     }
 
-    IEnumerator Fade() {
+    IEnumerator Fade()
+    {
         while (!Mathf.Approximately(Mathf.Round(curAlpha*10f)/10f, targetAlpha)) {
             curAlpha = Mathf.Lerp(curAlpha, targetAlpha, Time.deltaTime);
             cGroup.alpha = curAlpha;
             Debug.Log("Alpha: " + curAlpha);
             yield return new WaitForSeconds(Time.deltaTime);
         }
+        gameObject.SetActive(false);
         finishedFade.Invoke();
     }
 }
