879819568b71367c39091d0db3edafe9a7b1671a
Joseph Despain <jspankin5@gmail.com>
2018-02-27 19:49:53 -0700

    Options Spawner (For Editor)

---
+++ KickshotProject/Assets/Scripts/Managers/OptionSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionSpawner : MonoBehaviour {
+
+    public GameObject ObjManager;
+	// Use this for initialization
+	void Start () {
+        GameObject obj = GameObject.Find("[Options_Manager]");
+
+        if (obj == null)
+        {
+            obj = Instantiate(ObjManager) as GameObject;
+            obj.name = "[Options_Manager]";
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+}
