4cb3e3106c36ed23558db7fe068ac761fe123b88
Monish Gupta <monish1478@gmail.com>
2018-03-26 00:24:23 -0600

    Fixed some bugs with the options menu and leaderboard panel is coming out.

---
+++ KickshotProject/Assets/Scripts/Managers/HighscoreManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreManager : MonoBehaviour
+{
+    public InGameGUIManager guiManager;
+
+    bool Active;
+
+    public void Awake()
+    {
+        Active = false;
+    }
+
+    public void FadeOut(GameObject menu)
+    {
+        if (guiManager == null)
+        {
+            Active = false;
+            Animator anim = menu.GetComponent<Animator>();
+            anim.SetTrigger("FadeOut2");
+            return;
+        }
+
+        if (guiManager.optionsOpen)
+        {
+            guiManager.optionsOpen = false;
+            Active = false;
+            Animator anim = menu.GetComponent<Animator>();
+            anim.SetTrigger("FadeOut2");
+        }
+        else
+        {
+            FadeIn(menu);
+        }
+    }
+
+    public void FadeIn(GameObject menu)
+    {
+        if (guiManager == null)
+        {
+            Active = true;
+            Animator anim = menu.GetComponent<Animator>();
+            anim.SetTrigger("FadeIn2");
+            return;
+        }
+
+        if (!guiManager.optionsOpen)
+        {
+            guiManager.optionsOpen = true;
+            Active = true;
+            Animator anim = menu.GetComponent<Animator>();
+            anim.SetTrigger("FadeIn2");
+        }
+        else
+        {
+            FadeOut(menu);
+        }
+    }
+
+    public void Activate()
+    {
+        CanvasGroup group = gameObject.GetComponent<CanvasGroup>();
+        group.alpha = 1;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    public void Deactivate()
+    {
+        CanvasGroup group = gameObject.GetComponent<CanvasGroup>();
+        group.alpha = 0;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+        Active = false;
+    }
+}
--- KickshotProject/Assets/Scripts/UI/InGameGUIManager.cs
+++ KickshotProject/Assets/Scripts/UI/InGameGUIManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InGameGUIManager : MonoBehaviour {
     public bool activeMenu { get; set; } // this tells us whether the win/lose screens are active so we can prevent pausing if so.
     public bool optionsOpen { get; set; } // this tells us whether the options menu is opened or not
+    public bool scoresOpen { get; set; }
     public bool showCursor = false;
 
     public void OnGUI()
